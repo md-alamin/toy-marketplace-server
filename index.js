95364b824ddde5b683f097f1531a0feaa8095e2e
md-alamin <md.alamin.madrista@gmail.com>
2023-05-20 03:32:35 +0600

    api created to find a single toy

--- index.js
+++ index.js
@@ -71,6 +71,14 @@ async function run() {
 			res.send(result);
 		});
 
+		// find a toy to show details
+		app.get('/toy/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await toysCollection.findOne(query);
+			res.send(result);
+		});
+
 		// user
 		app.get('/user/:email', async (req, res) => {
 			const email = req.params.email;
